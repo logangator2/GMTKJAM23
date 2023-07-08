23290784c1d2c3277e30e481bafdacdb0d8f5815
Maxwell Logan <32340228+logangator2@users.noreply.github.com>
2023-07-08 18:00:29 -0500

    Planting Audio
    +: Added and hooked up planting audio for when new child player is placed
    ~: Fixed a typo

--- Scenes/highlight.gd
+++ Scenes/highlight.gd
@@ -9,13 +9,13 @@ var clicked_coords : Vector2i = Vector2i(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	place_plant_sig.connect(get_parent().place_callable)
-	var vec2_coord = transform.origin
-	highlight_coords = Vector2i(vec2_coord)
-	highlight_coords -= Vector2i(tile_size/2, tile_size/2)
-	highlight_coords = highlight_coords / Vector2i(tile_size, tile_size)
+    place_plant_sig.connect(get_parent().place_callable)
+    var vec2_coord = transform.origin
+    highlight_coords = Vector2i(vec2_coord)
+    highlight_coords -= Vector2i(tile_size/2, tile_size/2)
+    highlight_coords = highlight_coords / Vector2i(tile_size, tile_size)
 
 
 func _on_button_pressed():
-	clicked_coords = highlight_coords
-	place_plant_sig.emit(clicked_coords)
+    clicked_coords = highlight_coords
+    place_plant_sig.emit(clicked_coords)
--- Scripts/in_game_ui.gd
+++ Scripts/in_game_ui.gd
@@ -2,18 +2,18 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    pass
 
 func _on_settings_button_pressed():
-	
-	pass # Replace with function body.
+    
+    pass # Replace with function body.
 
 
 func _on_end_turn_button_pressed():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
