23fe1143ff9f6b2da70ca235271b8dde834a2d47
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2023-07-08 16:56:53 -0400

    plant playerssssss

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,17 +3,17 @@ extends Node2D
 signal plant_sig
 
 func plant():
-    print("planting!")
-    plant_sig.emit()
+	print("planting!")
+	plant_sig.emit()
 
 func protect():
-    print("protecting!")
-    
+	print("protecting!")
+	
 func eat():
-    print("eating!")
-    
+	print("eating!")
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    print(get_node("/root/test_game/test_tilemap").plant_callable)
-    plant_sig.connect(get_node("/root/test_game/test_tilemap").plant_callable)
-    print(transform)
+	print(get_node("/root/test_game/test_tilemap").plant_callable)
+	plant_sig.connect(get_node("/root/test_game/test_tilemap").plant_callable)
+	print(transform)
