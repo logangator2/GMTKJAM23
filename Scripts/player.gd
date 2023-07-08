91db1a883ca7dc94ad619f38dbfb3763a736f2fb
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2023-07-07 21:14:13 -0400

    Revert "spawn player and crops"

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,9 +3,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(transform)
-	add_to_group("player")
-	
+    print(transform)
+    add_to_group("self")
+    
 
 
-
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    pass
