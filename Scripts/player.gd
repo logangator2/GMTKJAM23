01742bd1caf8ad859a986d587db9be850e3c26b3
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2023-07-09 01:01:16 -0400

    old mcdonald had a brain EIEIO

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,23 +9,23 @@ var plant_coord : Vector2i
 var resiliance : int = 1
 
 func plant():
-    print("planting!")
-    plant_sig.emit()
+	print("planting!")
+	plant_sig.emit()
 
 func protect():
-    print("protecting!")
-    protect_sig.emit()
-    
+	print("protecting!")
+	protect_sig.emit()
+	
 
-    
+	
 func play_sound():
-    $PlantingAudio.play()
+	$PlantingAudio.play()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    plant_sig.connect(get_node("/root/test_game/test_tilemap").plant_callable)
-    protect_sig.connect(get_node("/root/test_game/test_tilemap").protect_callable)
-    var vec2_coord = transform.origin
-    plant_coord = Vector2i(vec2_coord)
-    plant_coord -= Vector2i(tile_size/2, tile_size/2)
-    plant_coord = plant_coord / Vector2i(tile_size, tile_size)
+	plant_sig.connect(get_node("/root/test_game/test_tilemap").plant_callable)
+	protect_sig.connect(get_node("/root/test_game/test_tilemap").protect_callable)
+	var vec2_coord = transform.origin
+	plant_coord = Vector2i(vec2_coord)
+	plant_coord -= Vector2i(tile_size/2, tile_size/2)
+	plant_coord = plant_coord / Vector2i(tile_size, tile_size)
