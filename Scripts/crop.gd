85f62219dadee9a8e4fe9f0f39457c9e48850a7f
Maxwell Logan <32340228+logangator2@users.noreply.github.com>
2023-07-08 21:47:30 -0500

    Win Screen
    +: Added win screen
    +: Connected win screen

--- Scripts/crop.gd
+++ Scripts/crop.gd
@@ -7,14 +7,19 @@ var tile_size : int = 64
 var crop_coord : Vector2i
 
 func eat():
-	print("eating!")
-	eat_sig.emit()
-	
+    print("eating!")
+    # Test for no more crops
+    var tile_node = get_parent()
+    var crop_spaces : Array = tile_node.crop_spaces
+    if crop_spaces.size() == 0:
+        get_tree().change_scene_to_file("res://Scenes/win.tscn")
+    eat_sig.emit()
+    
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	eat_sig.connect(get_node("/root/test_game/test_tilemap").eat_callable)
-	var vec2_coord = transform.origin
-	crop_coord = Vector2i(vec2_coord)
-	crop_coord -= Vector2i(tile_size/2, tile_size/2)
-	crop_coord = crop_coord / Vector2i(tile_size, tile_size)
+    eat_sig.connect(get_node("/root/test_game/test_tilemap").eat_callable)
+    var vec2_coord = transform.origin
+    crop_coord = Vector2i(vec2_coord)
+    crop_coord -= Vector2i(tile_size/2, tile_size/2)
+    crop_coord = crop_coord / Vector2i(tile_size, tile_size)
 
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -4,15 +4,15 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$TextureRect.set_size(get_viewport().size)
+    $TextureRect.set_size(get_viewport().size)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if $AudioStreamPlayer2D.playing == false:
-		$AudioStreamPlayer2D.play()
+    if $AudioStreamPlayer2D.playing == false:
+        $AudioStreamPlayer2D.play()
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_packed(play_scene)
+    get_tree().change_scene_to_packed(play_scene)
 
 func _on_quit_button_pressed():
-	get_tree().quit()
+    get_tree().quit()
