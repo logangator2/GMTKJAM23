b49c9fd878d4aa75e6048da0d72d9497842a5373
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2023-07-09 02:52:10 -0400

    fix plant bug

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -5,18 +5,18 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    $TextureRect.set_size(get_viewport().size)
+	$TextureRect.set_size(get_viewport().size)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    if $AudioStreamPlayer2D.playing == false:
-        $AudioStreamPlayer2D.play()
+	if $AudioStreamPlayer2D.playing == false:
+		$AudioStreamPlayer2D.play()
 
 func _on_play_button_pressed():
-    get_tree().change_scene_to_packed(play_scene)
+	get_tree().change_scene_to_packed(play_scene)
 
 func _on_quit_button_pressed():
-    get_tree().quit()
+	get_tree().quit()
  
 func _on_credits_button_pressed():
-    get_tree().change_scene_to_packed(credits_scene)
+	get_tree().change_scene_to_packed(credits_scene)
